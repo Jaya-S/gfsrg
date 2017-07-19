bd6f4b13ff3bd9e3041a4de2fc9c793158742829
Jaya-S <jaivedgiri@gmail.com>
2017-07-19 21:01:06 +0530

    Create evenno.java

---
+++ evenno.java
@@ -0,0 +1,15 @@
+# gfsrg
+ import java.util.Scanner;
+public class evenno {
+  public static void main(String []args){
+    Scanner s=new Scanner (System.in);
+    int h=50;
+    System.out.println("printing  the even  numbers between 1 and" +h);
+    for(int i=1;i<=h;i++){
+        if(i%2==0){
+          System.out.println("i");
+        }
+    }
+    
+  }
+}
